47e1d0b45dbd78a0476a6a52e6329ba21a91a7fb
Aswini J <aswini.jan2002@gmail.com>
2026-08-15 17:39:30 +0530

    "dialog handling"

--- tests/sandBoxAdvanced.spec.ts
+++ tests/sandBoxAdvanced.spec.ts
@@ -14,5 +14,32 @@ test.describe('basic sandbox', () => {
         await expect(page.getByText('Selected: Weekday Batch').first()).toBeVisible();
     })
 
+
+    test('handle alert dialog', async ({ page }) => {
+        await page.goto(advancedUrl);
+        await page.once('dialog', async dialog => {
+            console.log(`Dialog message: ${dialog.message()}`);
+            await dialog.accept();
+        })
+        await page.getByTestId('alert-btn').click();
+        await expect(page.getByText('Alert handled.')).toBeVisible();
+
+
+        await page.once('dialog', async dialog => {
+            console.log(`Dialog message: ${dialog.message()}`);
+            await dialog.dismiss();
+        })
+        await page.getByTestId('confirm-btn').click();
+        await expect(page.getByText('Confirm dismissed.')).toBeVisible();
+
+        await page.once('dialog', async dialog => {
+            console.log(`Dialog message: ${dialog.message()}`);
+            await dialog.accept('playwright');
+        })
+        await page.getByTestId('prompt-btn').click();
+        await expect(page.getByText('Prompt value: playwright')).toBeVisible();
+
+    });
+
 }
 )
