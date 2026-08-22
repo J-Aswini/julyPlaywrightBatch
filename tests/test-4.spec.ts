8c3cbe15598c05457e0d603bbfb3fc964d5b77eb
Aswini J <aswini.jan2002@gmail.com>
2026-08-22 17:06:27 +0530

    "tableAndPagination and hooks"

--- tests/test-4.spec.ts
+++ tests/test-4.spec.ts
@@ -1,8 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
+test.beforeEach(async({page})=>{
+    await page.goto('https://www.saucedemo.com/');
+})
+test('test 1', async ({ page }) => {
   await page.locator('[data-test="username"]').click();
   await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="login-button"]').click();
+});
+
+test('test 2', async ({ page }) => {
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('problem_user');
   await page.locator('[data-test="login-button"]').click();
 });
